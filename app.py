7bee68594be42d2c56257af653c76fdb06940b57
Jignesh Modi <jigneshmodi.developer@gmail.com>
2020-06-13 11:46:03 -0700

    Feat: Updated python server for mainline and basline algo

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import os
 from flask import Flask, jsonify, request
 import algorithm
+import baseline
 
+mainline = algorithm.Robot()
+baseline = baseline.Robot()
 
-robot = algorithm.Robot()
 app = Flask(__name__)
 
-#textapi http://127.0.0.1:7500/?sequence[]=sm_right&sequence[]=lr_right&sequence[]=cr
+#textapi http://127.0.0.1:8080/?sequence[]=sm_right&sequence[]=lr_right&sequence[]=cr&algo=mainline
 @app.route('/')
 def do_GET():
     args = request.args
     sequence = args.getlist('sequence[]')
+    algo = args.get("algo")
     sequence = [str(action) for action in sequence]
     print(sequence)
-    result = robot.predict(sequence)
+    if algo == "baseline":
+        print("baseline")
+        result = baseline.predict(sequence)
+    else: 
+        print("mainline")
+        result = mainline.predict(sequence)
+    
     print(result)
-
     return jsonify({"next": result})
 
-
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 7500))
+    PORT = int(os.environ.get("PORT", 8080))
     app.run(port=PORT)
